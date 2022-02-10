3024d5b105ecd0d08d6f63533577d076c4260e9f
N03M0TT3 <noemotte@gmail.com>
2022-02-10 08:49:04 +0100

    Database nullabilty solved

--- app/src/main/java/fr/imaginnovation/moneytracker/Database.kt
+++ app/src/main/java/fr/imaginnovation/moneytracker/Database.kt
@@ -14,11 +14,12 @@ class Database(context : Context) : SQLiteOpenHelper(context, DATABASE_NAME, nul
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        db.execSQL(databaseInfo.SQL_CREATE_TABLE_QUERY)
+
+        db?.execSQL(databaseInfo.SQL_CREATE_TABLE_QUERY)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        db.execSQL(databaseInfo.SQL_DELETE_TABLE_QUERY)
+        db?.execSQL(databaseInfo.SQL_DELETE_TABLE_QUERY)
         onCreate(db)
     }
 
